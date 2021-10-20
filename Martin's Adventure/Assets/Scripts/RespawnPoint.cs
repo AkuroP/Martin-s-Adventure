eb7edf2285c64f140d95a9b9e845c6080a2268d4
AntoineEsbelin <74203837+AntoineEsbelin@users.noreply.github.com>
2021-10-20 15:45:58 +0100

    Prototype1
    Premier prototype de perso et de respawn

---
+++ Martin's Adventure/Assets/Scripts/RespawnPoint.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnPoint : MonoBehaviour
+{
+   
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform respawnPoint;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        player.transform.position = respawnPoint.transform.position;
+    }
+
+}
