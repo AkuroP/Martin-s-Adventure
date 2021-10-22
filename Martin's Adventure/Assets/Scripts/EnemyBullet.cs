dbd2bb171a254496bec01a643f4fd1d735f3d400
AkuroP <thomas.kang2002@gmail.com>
2021-10-22 14:28:23 +0200

    Conflict resolution
    conflict resolution of scripts "Bullet"

--- Martin's Adventure/Assets/Scripts/EnemyBullet.cs
+++ Martin's Adventure/Assets/Scripts/EnemyBullet.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
+public class EnemyBullet : MonoBehaviour
 {
     public float speed;
     public Vector2 dir;
